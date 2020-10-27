2feedba24ef14b61e823121aacd41636be3890e1
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-10-27 20:09:34 +0200

    1-7 tasks done

--- Hometask-5__26-10-2020/script.js
+++ Hometask-5__26-10-2020/script.js
@@ -1,17 +1,17 @@
 // // task 1
-let str1 = 'hello';
-let str2 = 'owu';
-let str3 = 'com';
-let str4 = 'ua';
-let number1 = 1;
-let number2 = 10;
-let number3 = -999;
-let number4 = 123;
-let number5 = 3.14;
-let number6 = 2.7;
-let number7 = 16;
-let boolean1 = true;
-let boolean2 = false;
+// let str1 = 'hello';
+// let str2 = 'owu';
+// let str3 = 'com';
+// let str4 = 'ua';
+// let number1 = 1;
+// let number2 = 10;
+// let number3 = -999;
+// let number4 = 123;
+// let number5 = 3.14;
+// let number6 = 2.7;
+// let number7 = 16;
+// let boolean1 = true;
+// let boolean2 = false;
 
 // console.log(str1);
 // document.write(str1, '<br>');
@@ -64,6 +64,158 @@ let boolean2 = false;
 // console.log(boolean2);
 // document.write(boolean2, '<br>');
 // alert(boolean2);
+
+
+
+// // task 2
+// str1 = 'hello world';
+// str2 = 'owuu';
+// str3 = 'comercial';
+// str4 = 'ukraine';
+// number1 = 12;
+// number2 = 100;
+// number3 = -99990;
+// number4 = 12345;
+// number5 = 3.1456;
+// number6 = 2.777;
+// number7 = 160;
+// boolean1 = false;
+// boolean2 = true;
+//
+// console.log(str1);
+// document.write(str1, '<br>');
+// alert(str1);
+//
+// console.log(str2);
+// document.write(str2, '<br>');
+// alert(str2);
+//
+// console.log(str3);
+// document.write(str3, '<br>');
+// alert(str3);
+//
+// console.log(str4);
+// document.write(str4, '<br>');
+// alert(str4);
+//
+// console.log(number1);
+// document.write(number1, '<br>');
+// alert(number1);
+//
+// console.log(number2);
+// document.write(number2, '<br>');
+// alert(number2);
+//
+// console.log(number3);
+// document.write(number3, '<br>');
+// alert(number3);
+//
+// console.log(number4);
+// document.write(number4, '<br>');
+// alert(number4);
+//
+// console.log(number5);
+// document.write(number5, '<br>');
+// alert(number5);
+//
+// console.log(number6);
+// document.write(number6, '<br>');
+// alert(number6);
+//
+// console.log(number7);
+// document.write(number7, '<br>');
+// alert(number7);
+//
+// console.log(boolean1);
+// document.write(boolean1, '<br>');
+// alert(boolean1);
+//
+// console.log(boolean2);
+// document.write(boolean2, '<br>');
+// alert(boolean2);
+
+
+// // task 3
+// const strOne = 'string one';
+// const strTwo = 'string two';
+// const strThree = 'string three';
+// const numOne = 1;
+// const numTwo = 2;
+// const numThree = 3;
+//
+// console.log(strOne);
+// document.write(strOne, '<br>');
+// alert(strOne);
+//
+// console.log(strTwo);
+// document.write(strTwo, '<br>');
+// alert(strTwo);
+//
+// console.log(strThree);
+// document.write(strThree, '<br>');
+// alert(strThree);
+//
+// console.log(numOne);
+// document.write(numOne, '<br>');
+// alert(numOne);
+//
+// console.log(numTwo);
+// document.write(numTwo, '<br>');
+// alert(numTwo);
+//
+// console.log(numThree);
+// document.write(numThree, '<br>');
+// alert(numThree);
+
+
+// // task 4
+
+// let name = prompt('Enter your name!');
+// let surname = prompt('Enter your surname!');
+// let otchestvo = prompt('Enter your otchestvo!');
+//
+// console.log(name);
+// document.write(name, '<br>');
+// alert(name);
+//
+// console.log(surname);
+// document.write(surname, '<br>');
+// alert(surname);
+//
+// console.log(otchestvo);
+// document.write(otchestvo, '<br>');
+// alert(otchestvo);
+
+
+
+// task 5
+
+// let person = surname + ' ' + name + ' ' + otchestvo;
+// console.log(person);
+
+// task 6
+
+// let myName = prompt('Enter your name!');
+// let mySurname = prompt('Enter your surname!');
+// let myOtchestvo = prompt('Enter your otchestvo!');
+//
+// let myFatherName = prompt('Enter your father name!');
+// let myFatherSurname = prompt('Enter your father surname!');
+// let myFatherOtchestvo = prompt('Enter your father otchestvo!');
+//
+// let iAm = mySurname + ' ' + myName + ' ' + myOtchestvo;
+// let myFather = myFatherName + ' ' + myFatherSurname + ' ' + myFatherOtchestvo;
+//
+//
+// console.log(iAm);
+// document.write(iAm, '<br>');
+// alert(iAm);
+//
+// console.log(myFather);
+// document.write(myFather, '<br>');
+// alert(myFather);
+
+
 
 // // task 7
 // let number1 = prompt();
