276ce76626bdcdb032a5fa9c6aea098218f63837
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-10-27 21:25:59 +0200

    block Class done

--- Hometask-5__26-10-2020/script.js
+++ Hometask-5__26-10-2020/script.js
@@ -66,7 +66,6 @@
 // alert(boolean2);
 
 
-
 // // task 2
 // str1 = 'hello world';
 // str2 = 'owuu';
@@ -187,7 +186,6 @@
 // alert(otchestvo);
 
 
-
 // task 5
 
 // let person = surname + ' ' + name + ' ' + otchestvo;
@@ -202,9 +200,14 @@
 // let myFatherName = prompt('Enter your father name!');
 // let myFatherSurname = prompt('Enter your father surname!');
 // let myFatherOtchestvo = prompt('Enter your father otchestvo!');
+
+// let myMotherName = prompt('Enter your mother name!');
+// let myMotherSurname = prompt('Enter your mother surname!');
+// let myMotherOtchestvo = prompt('Enter your mother otchestvo!');
 //
 // let iAm = mySurname + ' ' + myName + ' ' + myOtchestvo;
 // let myFather = myFatherName + ' ' + myFatherSurname + ' ' + myFatherOtchestvo;
+// let myMother = myMotherName + ' ' + myMotherSurname + ' ' + myMotherOtchestvo;
 //
 //
 // console.log(iAm);
@@ -215,6 +218,9 @@
 // document.write(myFather, '<br>');
 // alert(myFather);
 
+// console.log(myMother);
+// document.write(myMother, '<br>');
+// alert(myMother);
 
 
 // // task 7
@@ -225,3 +231,170 @@
 // number2 = +number2;
 // number3 = +number3;
 // console.log(number1, number2, number3);
+
+
+//   CLASS
+
+// 1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+// При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+
+// let str = 'Привет';
+// let num = 123;
+// let flag = true;
+// let txt = 'true';
+//
+// console.log(str, typeof str);
+// console.log(num, typeof num);
+// console.log(flag, typeof flag);
+// console.log(txt, typeof txt);
+
+
+// 2. Создайте переменные a1, a2, a3, a4, a5. При помощи математических операторов (сложение, вычитание и т.д.) найдите значения выражений:
+//     5 + 3,
+//     5 - 3,
+//     5 * 3,
+//     5 / 3,
+//     5 % 3
+// поместив результат каждого выражения в соответствующую переменную. Например, let a1 = 5 + 3.
+
+// let a1 = 5 + 3;
+// console.log(a1);
+//
+// let a2 = 5 - 3;
+// console.log(a2);
+//
+// let a3 = 5 * 3;
+// console.log(a3);
+//
+// let a4 = 5 / 3;
+// console.log(a4);
+//
+// let a5 = 5 % 3;
+// console.log(a5);
+
+
+// 3.  Создайте переменные a6, a7, a8, a9, a10. Поместите в них результат выражений:
+//     5 % 3,
+//     3 % 5,
+//     5 + '3',
+//     '5' - 3,
+//     75 + 'кг'
+
+// let a6 = 5 % 3;
+// console.log(a6);
+//
+// let a7 = 3 % 5;
+// console.log(a7);
+//
+// let a8 = 5 + '3';
+// console.log(a8);
+//
+// let a9 = '5' - 3;
+// console.log(a9);
+//
+// let a10 = 75 + 'кг';
+// console.log(a10);
+
+
+// 4. Напишите код, который находит площадь прямоугольника высота 23см. ( переменная height),
+// шириной 10см (переменная width), значение площади должно хранится в числовой переменной s.
+
+// let height = 23;
+// let width = 10;
+// let s = height * width;
+// console.log(s);
+
+
+// 5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
+//     результат поместите в переменную v.
+
+// V = π R2 h
+// R = radius
+// h = heightC
+
+// let heightC = 10;
+// let dC = 4;
+// let radius = dC / 2;
+// let p = Math.PI;
+// let v = p * (radius ** 2) * heightC;
+// console.log(v);
+
+
+// 6. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
+// Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).
+
+// let n = 3;
+// let m = 4;
+// let k = Math.pow(n, 2) + Math.pow(m, 2);
+// console.log(k);
+
+
+// 7. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write, alert и console.log
+
+// let str = 'Hello world';
+// document.write(str);
+// alert(str);
+// console.log(str);
+
+
+// 8. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).
+
+// alert('Чорній Юрій Ігорович \n 22 \n skiing');
+
+
+// 9. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+//     Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
+//   Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+//     Вывести в документ содержимое переменной concatenation спомощью document.write
+
+// let str1 = 'Кто ';
+// let str2 = 'ты ';
+// let str3 = 'такой?';
+// let concatenation = str1 + str2 + str3;
+// document.write(concatenation);
+
+
+// // 10. Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); //205 - тому, що + спрацював як оператор конкатинації
+// document.write(str - a + "<br/>"); //15 - тому, що - в даному випадку перетворює стрінгу в намбер і тоді проводить операцію віднімання
+// document.write(str * "2" + "<br/>"); //40 - оператор множення теж приводить стрінгу в намбер і тоді проводить операцію множення
+// document.write(str / 2 + "<br/>"); //10 - оператор множення працює аналогічно відніманню і множенню (приводить типи до одного - намбера)
+
+
+// 11. Какие значения выведет в окно консоли следующий фрагмент кода если его поместить в console.log?
+//     parseInt("3.14")
+//     parseInt("-7.875")
+// parseInt("435")
+// parseInt("Вася")
+
+// console.log(parseInt("3.14")); //3
+// console.log(parseInt("-7.875")); //-7
+// console.log(parseInt("435")); //435
+// console.log(parseInt("Вася")); //NaN
+
+
+// 12.  С помощью окна ввода, вызываемого методом prompt, пользователь может ввести данные, которые будут использоваться далее, повторите код ниже
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+
+// 13. С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата при помощи метода alert
+
+// let number1 = +prompt('Введіть число:');
+// let number2 = +prompt('Введіть друге число:');
+// alert(number1 + number2);
+
+
+// 14. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и возраст, а вам не обходимо вывести строку такого вида
+// Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+
+// let name = prompt("Введіть ім'я");
+// let surname = prompt("Введіть прізвище");
+// let age = prompt("Скільки Вам років?");
+// alert('Доброго вечера ' + name + ' ' + surname + ', мои поздравления что вам ' + age);
+
