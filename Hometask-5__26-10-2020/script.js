ab159c3d1abfed025e7e5e29da919a2ad6805908
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-10-27 19:34:28 +0200

    first and 7 tasks done

---
+++ Hometask-5__26-10-2020/script.js
@@ -0,0 +1,75 @@
+// // task 1
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+let boolean1 = true;
+let boolean2 = false;
+
+// console.log(str1);
+// document.write(str1, '<br>');
+// alert(str1);
+//
+// console.log(str2);
+// document.write(str2, '<br>');
+// alert(str2);
+//
+// console.log(str3);
+// document.write(str3, '<br>');
+// alert(str3);
+//
+// console.log(str4);
+// document.write(str4, '<br>');
+// alert(str4);
+
+// console.log(number1);
+// document.write(number1, '<br>');
+// alert(number1);
+//
+// console.log(number2);
+// document.write(number2, '<br>');
+// alert(number2);
+//
+// console.log(number3);
+// document.write(number3, '<br>');
+// alert(number3);
+//
+// console.log(number4);
+// document.write(number4, '<br>');
+// alert(number4);
+//
+// console.log(number5);
+// document.write(number5, '<br>');
+// alert(number5);
+//
+// console.log(number6);
+// document.write(number6, '<br>');
+// alert(number6);
+//
+// console.log(number7);
+// document.write(number7, '<br>');
+// alert(number7);
+//
+// console.log(boolean1);
+// document.write(boolean1, '<br>');
+// alert(boolean1);
+//
+// console.log(boolean2);
+// document.write(boolean2, '<br>');
+// alert(boolean2);
+
+// // task 7
+// let number1 = prompt();
+// let number2 = prompt();
+// let number3 = prompt();
+// number1 = +number1;
+// number2 = +number2;
+// number3 = +number3;
+// console.log(number1, number2, number3);
