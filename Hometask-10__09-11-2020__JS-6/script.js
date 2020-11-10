3e938389d05260a219e981b02c1434791d55de02
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-11-10 21:02:25 +0200

    done

--- Hometask-10__09-11-2020__JS-6/script.js
+++ Hometask-10__09-11-2020__JS-6/script.js
@@ -1,6 +1,6 @@
 //
 // - создать массив с 20 числами.
-const myArray = [10, 2, 40, 3, 156, -1, 356, 1332, -12, 0, 12, 3, 15, 29, 34, 55, 23, 34, 124, 289]
+// const myArray = [10, 2, 40, 3, 156, -1, 356, 1332, -12, 0, 12, 3, 15, 29, 34, 55, 23, 34, 124, 289]
 
 // -- при помощи метода sort и колбека  отсортировать массив.
 // console.log(myArray.sort((a, b) => a - b));
@@ -212,19 +212,19 @@ const myArray = [10, 2, 40, 3, 156, -1, 356, 1332, -12, 0, 12, 3, 15, 29, 34, 55
 //
 //
 // - взять слдующий массив
-let usersWithAddress = [
-    {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 1}},
-    {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-    {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 90}},
-    {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-    {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
-    {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 22}},
-    {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-    {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 12}},
-    {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id: 11, name: 'max', age: 31, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}
-];
+// let usersWithAddress = [
+//     {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 1}},
+//     {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//     {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 90}},
+//     {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//     {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
+//     {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 22}},
+//     {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//     {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 12}},
+//     {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id: 11, name: 'max', age: 31, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}
+// ];
 // -- отсортировать его по id пользователей
 // console.log(usersWithAddress.sort((a, b) => a.id - b.id));
 
@@ -257,11 +257,34 @@ let usersWithAddress = [
 // }));
 
 // -- отсортировать его по названию улицы  в алфавитном порядке
+// console.log(usersWithAddress.sort((a, b) => {
+//     let streetA = a.address.street.toLowerCase();
+//     let streetB = b.address.street.toLowerCase();
+//     if (streetA < streetB) return -1;
+//     if (streetA > streetB) return 1;
+// }));
+
+
 // -- отсортировать его по номеру дома по возрастанию
+// console.log(usersWithAddress.sort((a, b) => a.address.number - b.address.number));
+
 // -- отфильтровать (оставить) тех кто младше 30
+// console.log(usersWithAddress.filter( value => value.age > 30))
+
+
 // -- отфильтровать (оставить) тех у кого отрицательный статус
+// console.log(usersWithAddress.filter(value => !value.status))
+
+
 // -- отфильтровать (оставить) тех у кого отрицательный статус и младше 30 лет
+// console.log(usersWithAddress.filter(value => !value.status && value.age < 30));
+
+
+
 // -- отфильтровать (оставить) тех у кого номер дома четный
+// console.log(usersWithAddress.filter(value => value.address.number % 2 === 0))
+
+
 //
 //
 // ===========================
@@ -279,6 +302,11 @@ let usersWithAddress = [
 //     Далі необхідно рати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на 10% та ціну на 5%
 // Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25, то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
 //     Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
+
+// цю задачку я вже зробив в одній із попередніх домашок!
+
+
+
 //
 //
 // Задача: дан отсортированный по возрастанию массив целых чисел. Необходимо вернуть наименьший и наибольший индекс заданного элемента.
@@ -286,8 +314,30 @@ let usersWithAddress = [
 //     Вывод: наибольший и наименьший индекс в массиве заданного элемента. Если такого элемента нет в массиве, выведите -1.
 //
 // Пример:
-//     Arr = [1, 2, 3, 4, 4, 4, 4, 7, 7, 9, 14]
+//     Arr = [1, 2, 3, 4, 4, 4, 4, 7, 7, 9];
 // 1. Key = 1
 // Answer: MinIndex = 0, MaxIndex = 0.
 // 2. Key = 4
 // Answer: MinIndex = 3, MaxIndex = 6.
+
+
+
+// let arr = [1, 2, 3, 4, 4, 4, 4, 7, 7, 9];
+//
+// function searchIndex(arr, key){
+//     // let min = null;
+//     // let max = null;
+//     // for (let i = 0; i < arr.length; i++) {
+//     //     if (arr[i] === key) {
+//     //         min === null ? min = i : max = i;
+//     //     }
+//     // }
+//     //
+//     // let str = ``;
+//     // return min === null ? -1 : str = `MinIndex = ${min}, MaxIndex = ${max}.`
+//
+//     return arr.indexOf(key) === -1 ? -1 :  `MinIndex = ${arr.indexOf(key)}, MaxIndex = ${arr.lastIndexOf(key)}.`
+//
+// }
+//
+// console.log(searchIndex(arr, 4));
