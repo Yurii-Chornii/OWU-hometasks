4a7fcfd67baaba7009b3fb28c18fc39010680462
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-11-10 17:24:15 +0200

    some tasks done

--- Hometask-10__09-11-2020__JS-6/script.js
+++ Hometask-10__09-11-2020__JS-6/script.js
@@ -1,6 +1,6 @@
 //
 // - создать массив с 20 числами.
-// const myArray = [10, 2, 40, 3, 156, -1, 356, 1332, -12, 0, 12, 3, 15, 29, 34, 55, 23, 34, 124, 289]
+const myArray = [10, 2, 40, 3, 156, -1, 356, 1332, -12, 0, 12, 3, 15, 29, 34, 55, 23, 34, 124, 289]
 
 // -- при помощи метода sort и колбека  отсортировать массив.
 // console.log(myArray.sort((a, b) => a - b));
@@ -25,7 +25,6 @@
 // console.log(newArray);
 
 
-
 //
 // - создать массив со словами на 15-20 элементов.
 // const words = ['ABETALIPOPROTEINEMIA', 'Anticholinesterase', 'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'deleniti', 'doloremque', 'illo', 'nam', 'odit', 'necessitatibus', 'Accusamus', 'voluptates'];
@@ -47,15 +46,81 @@
 //
 // Все робити через функції масивів (foreach, map ...тд)
 // Дан масив :
-//     let users = [ {name: 'vasya', age: 31, status: false}, {name: 'petya', age: 30, status: true}, {name: 'kolya', age: 29, status: true}, {name: 'olya', age: 28, status: false}, {name: 'max', age: 30, status: true}, {name: 'anya', age: 31, status: false}, {name: 'oleg', age: 28, status: false}, {name: 'andrey', age: 29, status: true}, {name: 'masha', age: 30, status: true}, {name: 'olya', age: 31, status: false}, {name: 'max', age: 31, status: true} ];
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
 // - відсортувати його за  віком (зростання , а потім окремо спадання)
+// console.log(users.sort((a, b) => a.age - b.age));
+// console.log(users.sort((a, b) => b.age - a.age));
+
+
 // - відсортувати його за кількістю знаків в імені  (зростання , а потім окремо спадання)
+// console.log(users.sort((a, b) => a.name.length - b.name.length));
+// console.log(users.sort((a, b) => b.name.length - a.name.length));
+
 // - пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний індентифікатор (По якому принципу його створювати - ваше рішення), та зберегти це в новий масив (первинний масив залишиться без змін)
+// let newUsers = JSON.stringify(users);
+// newUsers = JSON.parse(newUsers);
+// newUsers.forEach((item, index) => item.id = index + 1);
+// console.log(newUsers);
+
 // - відсортувати його за індентифікатором
+// console.log(newUsers.sort((a, b) => a.id - b.id));
+
 //
 //
-// -- наисать функцию калькулятора с 2мя числами и колбеком
-// -- наисать функцию калькулятора с 3мя числами и колбеком
+// -- написать функцию калькулятора с 2мя числами и колбеком
+// function plus2(a, b) {
+//     return a + b;
+// }
+// function minus2(a, b) {
+//     return a - b;
+// }
+// function multiplication2(a, b) {
+//     return a * b;
+// }
+// function division2(a, b) {
+//     return a / b;
+// }
+// function calc2(a, b, action){
+//     return action(a, b);
+// }
+// console.log(calc2(10, 10, plus2));
+// console.log(calc2(10, 10, minus2));
+// console.log(calc2(10, 10, multiplication2));
+// console.log(calc2(10, 10, division2));
+
+// -- написать функцию калькулятора с 3мя числами и колбеком
+// function plus3(a, b, c) {
+//     return a + b + c;
+// }
+// function minus3(a, b, c) {
+//     return a - b - c;
+// }
+// function multiplication3(a, b, c) {
+//     return a * b * c;
+// }
+// function division3(a, b, c) {
+//     return a / b / c;
+// }
+// function calc3(a, b, c, action){
+//     return action(a, b, c);
+// }
+// console.log(calc3(10, 10, 10, plus3));
+// console.log(calc3(10, 10, 10, minus3));
+// console.log(calc3(10, 10, 10, multiplication3));
+// console.log(calc3(10, 10, 10, division3));
+
 //
 // =============================================
 // =============================================
@@ -66,12 +131,43 @@
 //
 //
 //
-// let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250}, {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300}, {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140}, {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200}, {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165}, {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120}, {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120}, {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350}, {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180}, {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180}, {producer:"mercedes",model: "e63",year 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400}, {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
+let cars = [
+    {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400},
+    {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250},
+    {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300},
+    {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140},
+    {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200},
+    {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165},
+    {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120},
+    {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120},
+    {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350},
+    {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180},
+    {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180},
+    {producer:"mercedes",model: "e63",year: 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400},
+    {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230}
+];
 // Відфільтрувати масив за наступними крітеріями :
 //     - двигун більше 3х літрів
+// console.log(cars.filter(value => {
+//     if(value.volume > 3) return value;
+// }));
+
 // - двигун = 2л
+// console.log(cars.filter(value => {
+//     if(value.volume === 2) return value;
+// }));
+
+
 // - виробник мерс
+// console.log(cars.filter(value => {
+//     if(value.producer === 'mercedes') return value;
+// }));
+
 // - двигун більше 3х літрів + виробник мерседес
+// console.log(cars.filter(value => {
+//     if(value.producer === 'mercedes' && value.volume > 3) return value;
+// }));
+
 // - двигун більше 3х літрів + виробник субару
 // - сили більше ніж 300
 // - сили більше ніж 300 + виробник субару
