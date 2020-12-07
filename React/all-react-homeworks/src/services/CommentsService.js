a8226dfcbbacec824150526ec7c52251d083c233
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-12-07 18:10:43 +0200

    second commit work in All-comments.js

---
+++ React/all-react-homeworks/src/services/CommentsService.js
@@ -0,0 +1,15 @@
+export class CommentsService{
+    url = 'https://jsonplaceholder.typicode.com/comments';
+
+    getAllComments() {
+        return fetch(this.url)
+            .then(value => value.json())
+            .then(value => value)
+    }
+
+    getCommentById(id){
+        return fetch(`${this.url}/${id}`).then(value => value.json()).then(value => value)
+    }
+
+
+}
