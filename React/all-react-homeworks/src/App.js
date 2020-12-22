b259eea73a11029d582869dd6bef6a16e987295d
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-12-22 22:44:07 +0200

    counter

--- React/all-react-homeworks/src/App.js
+++ React/all-react-homeworks/src/App.js
@@ -1,14 +1,23 @@
-import React, {Component} from 'react';
 import './App.css';
+import React, {memo, useState, useMemo, useEffect, useCallback, useContext, useReducer, useRef, useLayoutEffect} from "react";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
+export default function App (){
+    const [count, setCount] = useState(0);
 
-            </div>
-        );
-    }
+    const incrementCounter = () => setCount((prev) => prev + 1);
+    const decrementCounter = () => setCount((prev) => prev - 1);
+    const cleanCounter = () => setCount(0);
+
+    useEffect(() => {
+        console.log('count wan changed', count)
+    }, [])
+
+    return (
+        <div>
+            <h1>Ви натиснули {count} разів</h1>
+            <button onClick={incrementCounter}>+</button>
+            <button onClick={decrementCounter}>-</button>
+            <button onClick={cleanCounter}>Очистити</button>
+        </div>
+    );
 }
-
-export default App;
