b8285682b9c40f08587963de757be62691a46592
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-12-25 11:38:22 +0200

    first commit cleanup function (useEffect)

--- React/all-react-homeworks/src/App.js
+++ React/all-react-homeworks/src/App.js
@@ -1,14 +1,32 @@
-import React, {Component} from 'react';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
 
-            </div>
-        );
-    }
+const Test = () => {
+    const [counter, setCounter] = useState(0);
+    useEffect(() => {
+        console.log('mounted')
+        return () => {
+            console.log('unmounted')
+        }
+    }, [counter])
+
+    return (
+        <h2 onClick={() => {setCounter((prev) => prev + 1)}}>Test component here</h2>
+    )
 }
 
-export default App;
+
+export default function App() {
+    const [isVisible, setIsVisible] = useState(false);
+    return (
+        <div>
+            <h1>App component here</h1>
+            <button onClick={() => {
+                setIsVisible(!isVisible)
+            }}>visible toggler
+            </button>
+            {isVisible && <Test/>}
+        </div>
+    );
+}
