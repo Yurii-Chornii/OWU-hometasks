3f9ca76ddeda633a09a31d7f77d9f20d6c744729
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-12-25 12:26:10 +0200

    second commit useMemo

--- React/all-react-homeworks/src/App.js
+++ React/all-react-homeworks/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useMemo} from "react";
 
 
 const Test = () => {
@@ -19,6 +19,16 @@ const Test = () => {
 
 export default function App() {
     const [isVisible, setIsVisible] = useState(false);
+    const [arr, setArr] = useState([2,12,57,3,1,1003,999,843,2500,20,115])
+
+    const totalPrice = arr.reduce((acc, val) => (acc += val), 0)
+    console.log(totalPrice)
+    const totalPrice2 = useMemo(() => {
+        console.log('from useMemo')
+        return arr.reduce((acc, val) => (acc += val), 0)
+    }, [arr])
+    console.log(totalPrice2)
+
     return (
         <div>
             <h1>App component here</h1>
@@ -27,6 +37,7 @@ export default function App() {
             }}>visible toggler
             </button>
             {isVisible && <Test/>}
+            <button onClick={() => {setArr([...arr, 5])}}>change arr for useMemo</button>
         </div>
     );
 }
