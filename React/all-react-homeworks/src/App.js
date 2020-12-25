9a6e2da5bed93a550b88331efe3c7c0dd4a3a9f4
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-12-25 16:35:24 +0200

    third commit redux

--- React/all-react-homeworks/src/App.js
+++ React/all-react-homeworks/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React from "react";
+import {useSelector, useDispatch} from 'react-redux';
 
 
 export default function App() {
+    const counter = useSelector(({counter}) => counter)
+    const dispatch = useDispatch();
+
     return (
         <div>
-            hello
+            hello, current value - {counter}
+            <br/>
+            <button onClick={() => dispatch({type: 'INC_COUNTER'})}>inc</button>
+            <button onClick={() => dispatch({type: 'DEC_COUNTER'})}>dec</button>
+            <button onClick={() => dispatch({type: 'RESET'})}>reset</button>
         </div>
     );
 }
--- React/all-react-homeworks/src/index.js
+++ React/all-react-homeworks/src/index.js
@@ -3,13 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    counter: 0,
+    user : {name: 'Yura', age: 22}
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC_COUNTER' : {
+            return {...state, counter: state.counter + 1}
+        }
+        case 'DEC_COUNTER' : {
+            return {...state, counter: state.counter - 1}
+        }
+        case 'RESET' : {
+            return {...state, counter: 0}
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+const store = createStore(reducer);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
