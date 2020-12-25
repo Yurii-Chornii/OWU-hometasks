c7044fb347c80c4625c4e42cbb8d4031a6124055
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-12-25 11:20:20 +0200

    counter

--- React/all-react-homeworks/src/App.js
+++ React/all-react-homeworks/src/App.js
@@ -1,23 +1,67 @@
 import './App.css';
-import React, {memo, useState, useMemo, useEffect, useCallback, useContext, useReducer, useRef, useLayoutEffect} from "react";
+import React, {useEffect, useState, useReducer} from "react";
 
-export default function App (){
-    const [count, setCount] = useState(0);
+// useReducer
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_USER': {
+            return action.payload
+        }
+        case 'CHANGE_USER_NAME': {
+            return {
+                ...state,
+                name : action.payload
+            }
+        }
+        case 'CHANGE_USER_ID': {
+            return {
+                ...state,
+                id : state.id + 1
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
+const initialState = {
+    id : null,
+    name : ''
+}
+// useReducer
+
+export default function App() {
+    const [count, setCount] = useState(1);
+    const [user, setUser] = useState();
 
     const incrementCounter = () => setCount((prev) => prev + 1);
-    const decrementCounter = () => setCount((prev) => prev - 1);
-    const cleanCounter = () => setCount(0);
+    const decrementCounter = () => {
+        setCount((prev) => prev > 1 ? prev - 1 : prev)
+    }
+    const cleanCounter = () => setCount(1);
 
     useEffect(() => {
-        console.log('count wan changed', count)
-    }, [])
+        fetch(`https://jsonplaceholder.typicode.com/users/${count}`)
+            .then(value => value.json())
+            .then(value => setUser(value))
+    }, [count])
+
+
+    // useReducer
+    const [state, dispatch] = useReducer(reducer, initialState);
+    // useReducer
 
     return (
         <div>
-            <h1>Ви натиснули {count} разів</h1>
+            <h1>Значення {count}</h1>
             <button onClick={incrementCounter}>+</button>
             <button onClick={decrementCounter}>-</button>
             <button onClick={cleanCounter}>Очистити</button>
+            {!!user &&
+            (<div>
+                <p>{user.id} - {user.name} </p>
+            </div>)
+            }
         </div>
     );
 }
