5e85e7a0a0d36cde0d2981f1bcddd56ba7ae4ebe
Yurii Chornii <chorniy.yuraa@gmail.com>
2021-02-24 17:42:01 +0200

    some changes

--- Node.js/2homework/app.js
+++ Node.js/2homework/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const expressHBS = require('express-handlebars');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname + 'views')));
+
+
+
+
+
+
+
+
+app.get('/hello', (req, res) => {
+    res.json([
+        {name: 'Dima'},
+        {name: 'Yura'},
+        {name: 'Andriy'},
+    ])
+})
 
 app.listen(5000, () => {
     console.log('App listen 5000')
