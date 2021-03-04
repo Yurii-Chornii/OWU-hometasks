85c9abcc2141bee070fc8dd3dc8f9d32814b2e36
Yurii Chornii <chorniy.yuraa@gmail.com>
2021-03-04 18:23:04 +0200

    created project

---
+++ JavaAdvance/lesson4/lesson4java/src/main/java/com/example/lesson4java/Lesson4javaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lesson4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson4javaApplication.class, args);
+    }
+
+}
---
+++ JavaAdvance/lesson4/lesson4java/src/test/java/com/example/lesson4java/Lesson4javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson4java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson4javaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
