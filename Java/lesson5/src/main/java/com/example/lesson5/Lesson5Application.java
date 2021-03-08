ebe4a4228874c916d406a63122d2e949455e4215
Yurii Chornii <chorniy.yuraa@gmail.com>
2021-03-08 08:46:57 +0200

    initial commit

---
+++ Java/lesson5/src/main/java/com/example/lesson5/Lesson5Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson5Application.class, args);
+    }
+
+}
---
+++ Java/lesson5/src/test/java/com/example/lesson5/Lesson5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
